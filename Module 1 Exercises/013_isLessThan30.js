e590b845dd0afa816397f27456d2bf6f9eec89f1
David Agustin <davidsyagustin@gmail.com>
2018-09-07 17:47:31 -0700

    Finish up to 43 exercises in Module 1

---
+++ Module 1 Exercises/013_isLessThan30.js
@@ -0,0 +1,15 @@
+/*
+Write a function called "isLessThan30".
+Given a number, "isLessThan30" returns whether the given number is less than 30.
+var output = isLessThan30(9);
+console.log(output); // --> true
+*/
+
+function isLessThan30(num) {
+  // your code here
+  if (num < 30){
+    return true
+  } else {
+    return false
+  }
+}
---
+++ Module 1 Exercises/015_isLessThan.js
@@ -0,0 +1,15 @@
+/*
+Write a function called "isLessThan".
+Given 2 numbers, "isLessThan" returns whether num2 is less than num1.
+var output = isLessThan(9, 4);
+console.log(output); // --> true
+*/
+
+function isLessThan(num1, num2) {
+  // your code here
+  if (num1 > num2) {
+    return true
+  } else {
+    return false
+  }
+}
