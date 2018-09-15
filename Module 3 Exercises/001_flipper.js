107aa8e0be0a4761210e3eee6dfd2d2a04761a05
David Agustin <davidsyagustin@gmail.com>
2018-09-15 06:05:48 -0700

    Refactor Exercises

--- Module 3 Exercises/001_flipper.js
+++ Module 3 Exercises/001_flipper.js
@@ -9,7 +9,7 @@ console.log(output); // --> hcce kuo toh wnietertsni ghtsip orlbmei ,si 't snias
 //Flip every pair of characters in a string.
 
 
-    //Example:
+    //Examzple:
 var input = 'check out how interesting this problem is, it\'s insanely interesting!';
 //var output = flipPairs(input);
 //console.log(output); // --> hcce kuo toh wnietertsni ghtsip orlbmei ,si 't sniasenyli tnreseitgn!
--- Module 3 Exercises/007_divideAndConquer.js
+++ Module 3 Exercises/007_divideAndConquer.js
@@ -1,26 +1,3 @@
-/*
-Binary search is a technique for very rapidly searching a sorted collection by cutting the search space in half at every pass.
-
-    Given a sorted array, such as this:
-[1, 3, 16, 22, 31, 33, 34]
-
-You can search for the value 31, as follows:
-
-    1) Pick the midpoint: 22.
-2) The value is higher than 22, so now consider only the right half of the previous array:
-    [...31, 33, 34]
-3) Pick the midpoint: 33
-4) The value is lower than 33, so now consider only the left half of the previous array:
-    [...31...]
-5) Pick the midpoint: 31
-6) You've hit the value.
-7) Return the index of the value.
-
-    Notes:
-* If you don't find the value, you can return null.
-* If at any point you calculate the index of the midpoint and get a fractional number, just round it down ("floor" it).
-*/
-
 function binarySearch(array, target){
     if (array.length === 0) {
         return null
@@ -33,11 +10,9 @@ function binarySearch(array, target){
         return binarySearch(array.slice(0 , mid), target)
     } else if (array[mid] < target) {
         // go right
-        searchRes = binarySearch(array.slice(mid + 1), target)
+        let searchRes = binarySearch(array.slice(mid + 1), target)
         return (searchRes === null) ? null : mid + 1 + searchRes
     }
 }
 
-let answer = binarySearch([1, 3, 16, 22, 31, 33, 34],16);
 
-console.log(answer);
